b5115453e1b2589d78f77949a65a8dc8a573519f
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-10-26 18:18:23 +0400

    Renamed file_path to files_path

--- audio_experiment.py
+++ audio_experiment.py
@@ -15,11 +15,11 @@ from audio_experiment_utils import *
 AUDIO_FILES_DIR_DEFAULT = os.path.join(os.getcwd(), "sounds")
 
 # TODO: Write a usage text and add these arguments there.
-# Calling without -file_path        will expect to find the default ./sounds directory.
-# Calling with   "-file_path mydir" will expect to find a           ./mydir  directory.
+# Calling without -files_path        will expect to find the default ./sounds directory.
+# Calling with   "-files_path mydir" will expect to find a           ./mydir  directory.
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-file_path", type = Path)
+parser.add_argument("-files_path", type = Path)
 parser.add_argument("-plot_all",         action='store_true')
 parser.add_argument("-plot_signals",     action='store_true')
 parser.add_argument("-plot_frequencies", action='store_true')
@@ -31,15 +31,15 @@ args = parser.parse_args()
 
 ############################## Command Argument Verification ##############################
 
-if args.file_path is not None and not args.file_path.exists():
-    raise FileNotFoundError("Provided path " + quote(str(args.file_path)) + " not found.")
+if args.files_path is not None and not args.files_path.exists():
+    raise FileNotFoundError("Provided path " + quote(str(args.files_path)) + " not found.")
 
 if not Path(AUDIO_FILES_DIR_DEFAULT).exists():
     raise FileNotFoundError("Default path 'sounds' not found.")
 
 ###########################################################################################
 
-PAR_AUDIO_FILES_DIR = args.file_path if args.file_path is not None else AUDIO_FILES_DIR_DEFAULT
+PAR_AUDIO_FILES_DIR  = args.files_path if args.files_path is not None else AUDIO_FILES_DIR_DEFAULT
 PAR_PLOT_FREQUENCIES = args.plot_all or args.plot_frequencies
 PAR_PLOT_SIGNALS     = args.plot_all or args.plot_signals
 PAR_PLOT_SPECS       = args.plot_all or args.plot_specs
