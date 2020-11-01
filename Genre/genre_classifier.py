a330a93b3846506a78210ca605120f3d740ffbe2
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-11-01 17:00:13 +0400

    Comments

--- Genre/genre_classifier.py
+++ Genre/genre_classifier.py
@@ -40,7 +40,7 @@ PAR_EPOCHS     = args.epochs     # default: 50 - number of epochs to train
 
 def load_data(data_path):
     """
-    Loads training data from json file.
+    Loads training data from json file and reads them into arrays for NN processing.
         :param data_path (str): Path to json file containing data
         :return inputs (ndarray)
         :return labels (ndarray)
