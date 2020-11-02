99e42f8bd6ec3af32aacbd9fe185debd8de5006d
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-11-02 12:35:53 +0400

    Added coloring print() forwarding functions.

--- common_utils.py
+++ common_utils.py
@@ -1,5 +1,6 @@
 import glob
 import os
+from termcolor import colored
 
 # TODO: Move these to common utils module
 def exists(x):
@@ -14,3 +15,32 @@ def quote(me):
 def get_most_recent_file_in_dir(data_json_path, ext):
     files = glob.glob(data_json_path + '/*.' + ext)
     return max(files, key = os.path.getctime)
+
+"""
+Available text colors:
+    red, green, yellow, blue, magenta, cyan, white.
+
+Available text highlights:
+    on_red, on_green, on_yellow, on_blue, on_magenta, on_cyan, on_white.
+
+Available attributes:
+    bold, dark, underline, blink, reverse, concealed.
+
+Example:
+    colored('Hello, World!', 'red', 'on_grey', ['blue', 'blink'])
+    colored('Hello, World!', 'green')
+"""
+def printy(s, **kwargs):
+    print(colored(s, 'yellow'), **kwargs)
+
+def printm(s, **kwargs):
+    print(colored(s, 'magenta'), **kwargs)
+
+def printc(s, **kwargs):
+    print(colored(s, 'cyan'), **kwargs)
+
+def printw(s, **kwargs):
+    print(colored(s, 'white'), **kwargs)
+
+def printr(s, **kwargs):
+    print(colored(s, 'red'), **kwargs)
