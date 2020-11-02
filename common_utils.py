b69ac5b1fd4b8a41f3f101e57864121bfd14bc48
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-11-02 13:20:14 +0400

    Added more coloring print() forwarding functions.

--- common_utils.py
+++ common_utils.py
@@ -16,6 +16,45 @@ def get_most_recent_file_in_dir(data_json_path, ext):
     files = glob.glob(data_json_path + '/*.' + ext)
     return max(files, key = os.path.getctime)
 
+class Colors:
+    PURPLE      = '\033[95m'
+    BLUE        = '\033[94m'
+    CYAN        = '\033[96m'
+    GREEN       = '\033[92m'
+    LIGHTYELLOW = '\033[93m'
+    PINK        = '\033[91m'
+    ENDC        = '\033[0m'
+    BOLD        = '\033[1m'
+    UNDERLINE   = '\033[4m'
+
+    def disable(self):
+        self.PURPLE      = ''
+        self.BLUE        = ''
+        self.GREEN       = ''
+        self.LIGHTYELLOW = ''
+        self.PINK        = ''
+        self.ENDC        = ''
+
+# Forwarding colored print functions using ASCII escape sequences
+def printP(s, *args, **kwargs):
+    print(f"{Colors.PURPLE}" + s + f"{Colors.ENDC}", *args, **kwargs)
+
+def printB(s, *args, **kwargs):
+    print(f"{Colors.BLUE}" + s + f"{Colors.ENDC}", *args, **kwargs)
+
+def printC(s, *args, **kwargs):
+    print(f"{Colors.CYAN}" + s + f"{Colors.ENDC}", *args, **kwargs)
+
+def printG(s, *args, **kwargs):
+    print(f"{Colors.GREEN}" + s + f"{Colors.ENDC}", *args, **kwargs)
+
+def printL(s, *args, **kwargs):
+    print(f"{Colors.LIGHTYELLOW}" + s + f"{Colors.ENDC}", *args, **kwargs)
+
+def printK(s, *args, **kwargs):
+    print(f"{Colors.PINK}" + s + f"{Colors.ENDC}", *args, **kwargs)
+
+# Forwarding colored print functions using termcolor Python module
 """
 Available text colors:
     red, green, yellow, blue, magenta, cyan, white.
@@ -44,3 +83,6 @@ def printw(s, *args, **kwargs):
 
 def printr(s, *args, **kwargs):
     print(colored(s, 'red'), *args, **kwargs)
+
+def printg(s, *args, **kwargs):
+    print(colored(s, 'green'), *args, **kwargs)
