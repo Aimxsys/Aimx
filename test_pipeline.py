601627204b68a1d316dbf647ddad0c4b6459e704
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-11-06 20:02:51 +0400

    PC+

--- test_pipeline.py
+++ test_pipeline.py
@@ -2,5 +2,4 @@ import subprocess
 
 subprocess.call(['genre_preprocess.py', '-dataset_path', 'dataset_c10_f3'], shell=True)
 
-#subprocess.call("genre_classifier.py -data_path most_recent_output -epochs 100", shell=True)
 subprocess.call(['genre_classifier.py', '-data_path', 'most_recent_output', '-epochs', '5'], shell=True)
