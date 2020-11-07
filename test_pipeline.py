075ba9f79460f2497b12ca05a756224643e4de28
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-11-07 16:18:37 +0400

    Added audex.py to test_pipeline.py

--- test_pipeline.py
+++ test_pipeline.py
@@ -7,6 +7,11 @@ import subprocess
 # which can result in arbitrary command execution. For this reason, the use of shell=True
 # is strongly discouraged in cases where the command string is constructed from external input
 
+# Audex
+subprocess.call(['audex.py'], shell=True)
+
+# Data preprocessing
 subprocess.call(['genre_preprocess.py', '-dataset_path', 'dataset_c10_f3'], shell=True)
 
+# Genre classification
 subprocess.call(['genre_classifier.py', '-data_path', 'most_recent_output', '-epochs', '5'], shell=True)
