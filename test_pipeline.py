62fea9fa59c7ef476a142deabb5d6a45c961fbbf
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-11-07 16:42:29 +0400

    Added "test ok" markers in test_pipeline.py log

--- test_pipeline.py
+++ test_pipeline.py
@@ -1,5 +1,7 @@
 import subprocess
 
+from common_utils import *
+
 # IMPORTANT: TODO: Note for later resolution from official Python documentation
 # https://docs.python.org/2/library/subprocess.html#frequently-used-arguments:
 # Warning Executing shell commands that incorporate unsanitized input from an untrusted
@@ -8,10 +10,18 @@ import subprocess
 # is strongly discouraged in cases where the command string is constructed from external input
 
 # Audex
-subprocess.call(['audex.py'], shell=True)
+subprocess.call(['audex.py', '-files_path', 'sounds/two'], shell=True)
+
+print(magenta("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT audex.py OK"))
 
 # Data preprocessing
 subprocess.call(['genre_preprocess.py', '-dataset_path', 'dataset_c10_f3'], shell=True)
 
+print(magenta("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT genre_preprocess.py OK"))
+
 # Genre classification
 subprocess.call(['genre_classifier.py', '-data_path', 'most_recent_output', '-epochs', '5'], shell=True)
+
+print(magenta("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT genre_classifier.py OK"))
+
+print(magenta("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT PIPELINE TEST OK"))
