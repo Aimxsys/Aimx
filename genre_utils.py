a40dc229c3907640878ccc6a81c0d7ec91a47883
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-11-10 22:49:18 +0400

    Removed "most_recent_" prefix

--- genre_utils.py
+++ genre_utils.py
@@ -74,7 +74,7 @@ def plot_history(history, trainid, show_interactive):
 
     # save the plot as most recent (often useful when comparing to a next NN run)
     Path("gen_plots").mkdir(parents=True, exist_ok=True)
-    mr_plot_fullpath = os.path.join("gen_plots", "most_recent_plot_" + trainid + get_dataset_code(dataset_json_filename) + ".png")
+    mr_plot_fullpath = os.path.join("gen_plots", "plot_" + trainid + get_dataset_code(dataset_json_filename) + ".png")
     print_info("\n|||||| Saving image file", quote(cyansky(mr_plot_fullpath)), "... ", end="")
     pt.savefig(mr_plot_fullpath)
     print_info("[DONE]")
@@ -83,7 +83,7 @@ def plot_history(history, trainid, show_interactive):
         pt.show()
 
 def save_current_model(model, model_id):
-    mr_model_fullpath = os.path.join("gen_saved_models", "most_recent_model_" + model_id)
+    mr_model_fullpath = os.path.join("gen_saved_models", "model_" + model_id)
     print_info("\n|||||| Saving model ", quote(cyansky(mr_model_fullpath)), "... ", end="")
     model.save(mr_model_fullpath)
     print_info("[DONE]")
