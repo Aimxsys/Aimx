18c75b12fed9d5bd9db2b7996b494925e66096d1
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-11-21 16:10:40 +0400

    Minor fix

--- Audex/sanitest.py
+++ Audex/sanitest.py
@@ -28,7 +28,7 @@ args = parser.parse_args()
 ARG_TEST_PLOT_SOUND      = args.all or args.plot_sound
 
 ARG_TEST_DATAPREP_ASR    = args.all or args.dataprep_asr
-ARG_TEST_TRAIN_ASR_CNN   = args.all or args.cnn
+ARG_TEST_TRAIN_ASR_CNN   = args.all or args.nns or args.cnn
 
 ARG_TEST_DATAPREP_GENRE  = args.all or args.dataprep_genre
 ARG_TEST_TRAIN_GENRE_ANN = args.all or args.nns or args.ann
