2f2ac06e45cceb7fcfdd547e26de9a73b24bce67
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-11-30 16:54:31 +0400

    Added clargs to PC b369ca5 where test_fullpipe_asr.py was added

--- Audex/test_fullpipe_asr.py
+++ Audex/test_fullpipe_asr.py
@@ -8,6 +8,7 @@ import subprocess
 
 from common_utils import *
 from audex_utils  import Aimx
+from audex_utils  import get_actual_model_path
 
 # TODO: Replace the hard-coded line below with automatically extracted dirlabels
 DATASET_VIEW_DEFAULT = ['alldirlabs']
@@ -21,6 +22,12 @@ parser.add_argument("-epochs",         default = 5,  type=int, help = 'Number of
 parser.add_argument("-inferdata_path", type = Path,            help = 'Path to the audio files on which model inference is to be tested.')
 
 args = parser.parse_args()
+
+########################## Command Argument Handling & Verification #######################
+
+# ...
+
+###########################################################################################
 
 print_info("###############################################")
 print_info("Running ASR full pipeline with the following arguments: ")
@@ -43,15 +50,18 @@ start_time = time.time()
 ####################################################### ASR-related pipeline
 
 # dataprep_asr.py -dataset_path ../workdir/speech_commands_v001 -dataset_depth 5
-subprocess.call(['dataprep_asr.py', '-dataset_path', '../workdir/speech_commands_v001', '-dataset_depth', str(args.dataset_depth)], shell=True)
+subprocess.call(['dataprep_asr.py', '-dataset_path', str(args.dataset_path), '-dataset_depth', str(args.dataset_depth)], shell=True)
 print(magenta("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT dataprep_asr.py OK"))
 
 # train_asr_cnn.py -traindata_path most_recent_output -epochs 5
-subprocess.call(['train_asr_cnn.py', '-traindata_path', Aimx.MOST_RECENT_OUTPUT, '-epochs', str(args.epochs)], shell=True)
+subprocess.call(['train_asr_cnn.py', '-traindata_path', Aimx.MOST_RECENT_OUTPUT, '-epochs', str(args.epochs), '-savemodel'], shell=True)
 print(magenta("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT train_asr_cnn.py OK"))
 
+model_path = "../workdir/gen_models/" + extract_filename(get_actual_model_path(Aimx.MOST_RECENT_OUTPUT))
+print(pinkred(model_path), pinkred("1"))
+
 # service_wordetect.py -model_path most_recent_output -inferdata_path ../workdir/infer_down
-subprocess.call(['service_wordetect.py', '-model_path', Aimx.MOST_RECENT_OUTPUT, '-inferdata_path', str(args.epochs)], shell=True)
+subprocess.call(['service_wordetect.py', '-model_path', Aimx.MOST_RECENT_OUTPUT, '-inferdata_path', str(args.inferdata_path)], shell=True)
 print(magenta("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT train_asr_cnn.py OK"))
 
 print(magenta("ꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕꓕ ASR FULL PIPELINE TEST OK"))
