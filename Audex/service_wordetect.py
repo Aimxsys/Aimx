609c326aa7a539d1a4258af3ea38f850b87fc8d1
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-12-07 18:11:34 +0400

    Prep to use in a client/server model

--- Audex/service_wordetect.py
+++ Audex/service_wordetect.py
@@ -8,31 +8,33 @@ from audex_utils  import Aimx
 from audex_utils  import get_dataprep_result_meta
 from audex_utils  import get_actual_model_path
 
-# Calling with "-inferdata_path /to/file" will expect to find the file in ./to directory.
-parser = argparse.ArgumentParser(description = 'This utility script allows you to experiment with'
-                                               ' audio files and their various spectrograms.')
+if __name__ == "__main__":
 
-parser.add_argument("-inferdata_path",       type = Path,               help = 'Path to the audio files on which model inference is to be tested.')
-parser.add_argument("-model_path",           type = Path,               help = 'Path to the model to be loaded.')
-parser.add_argument("-highlight_confidence", default = 0.9, type=float, help = 'Highlight results if confidence is higher than this threshold.')
+    # Calling with "-inferdata_path /to/file" will expect to find the file in ./to directory.
+    parser = argparse.ArgumentParser(description = 'This utility script allows you to experiment with'
+                                                   ' audio files and their various spectrograms.')
 
-parser.add_argument("-n_mfcc",         default =    13, type=int, help = 'Number of MFCC to extract.')
-parser.add_argument("-n_fft",          default =  2048, type=int, help = 'Length of the FFT window.   Measured in # of samples.')
-parser.add_argument("-hop_length",     default =   512, type=int, help = 'Sliding window for the FFT. Measured in # of samples.')
-parser.add_argument("-num_segments",   default =     5, type=int, help = 'Number of segments we want to divide sample tracks into.')
-parser.add_argument("-sample_rate",    default = 22050, type=int, help = 'Sample rate at which to read the audio files.')
-parser.add_argument("-track_duration", default =     1, type=int, help = 'Only load up to this much audio (in seconds).')
+    parser.add_argument("-inferdata_path",       type = Path,               help = 'Path to the audio files on which model inference is to be tested.')
+    parser.add_argument("-model_path",           type = Path,               help = 'Path to the model to be loaded.')
+    parser.add_argument("-highlight_confidence", default = 0.9, type=float, help = 'Highlight results if confidence is higher than this threshold.')
 
-args = parser.parse_args()
+    parser.add_argument("-n_mfcc",         default =    13, type=int, help = 'Number of MFCC to extract.')
+    parser.add_argument("-n_fft",          default =  2048, type=int, help = 'Length of the FFT window.   Measured in # of samples.')
+    parser.add_argument("-hop_length",     default =   512, type=int, help = 'Sliding window for the FFT. Measured in # of samples.')
+    parser.add_argument("-num_segments",   default =     5, type=int, help = 'Number of segments we want to divide sample tracks into.')
+    parser.add_argument("-sample_rate",    default = 22050, type=int, help = 'Sample rate at which to read the audio files.')
+    parser.add_argument("-track_duration", default =     1, type=int, help = 'Only load up to this much audio (in seconds).')
 
-############################## Command Argument Verification ##############################
+    args = parser.parse_args()
 
-if provided(args.inferdata_path) and not args.inferdata_path.exists():
-    raise FileNotFoundError("Directory " + quote(pinkred(os.getcwd())) + " does not contain requested path " + quote(pinkred(args.inferdata_path)))
+    ############################## Command Argument Verification ##############################
 
-###########################################################################################
+    if provided(args.inferdata_path) and not args.inferdata_path.exists():
+        raise FileNotFoundError("Directory " + quote(pinkred(os.getcwd())) + " does not contain requested path " + quote(pinkred(args.inferdata_path)))
 
-print_script_start_preamble(nameofthis(__file__), vars(args))
+    ###########################################################################################
+
+    print_script_start_preamble(nameofthis(__file__), vars(args))
 
 class _WordetectService:
     """
