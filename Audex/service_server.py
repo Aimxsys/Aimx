ff1d2bb7161cce09d1bbd6a8cc83f1bb687240ec
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-12-07 18:52:15 +0400

    Instantiate WDS with model clarg

--- Audex/service_server.py
+++ Audex/service_server.py
@@ -1,37 +1,58 @@
+import argparse
 import random
 import os
 
-from flask import Flask, request, jsonify
+from pathlib           import Path
+from flask             import Flask, request, jsonify
 from service_wordetect import CreateWordetectService
+from common_utils      import *
+from audex_utils       import get_actual_model_path
+
+# Calling with "-inferdata_path /to/file" will expect to find the file in ./to directory.
+parser = argparse.ArgumentParser(description = 'Inference service')
+
+parser.add_argument("-model_path", type = Path, help = 'Path to the model to be loaded.')
+
+args = parser.parse_args()
+
+############################## Command Argument Handling & Verification ##############################
+
+# ...
+
+######################################################################################################
+
+print_script_start_preamble(nameofthis(__file__), vars(args))
+
+args.model_path = get_actual_model_path(args.model_path)
 
 # instantiate flask app
 app = Flask(__name__)
 
 @app.route("/predict", methods=["POST"])
 def predict():
-	"""
-	Word detection endpoint
+    """
+    Word detection endpoint
     :return (json): This endpoint returns a json file with the following format:
         {
             "keyword": "down"
         }
-	"""
+    """
 
-	# get audio file from POST request and save it
-	audio_file = request.files["file"]
-	file_name  = str(random.randint(0, 100_000))
-	audio_file.save(file_name)
+    # get audio file from POST request and save it
+    audio_file = request.files["file"]
+    file_name  = str(random.randint(0, 100_000))
+    audio_file.save(file_name)
 
-	# instantiate keyword spotting service singleton and get prediction
-	wds = CreateWordetectService()
-	pred_word = wds.predict(file_name)
+    # instantiate keyword spotting service singleton and get prediction
+    wds = CreateWordetectService(args.model_path)
+    pred_word = wds.predict(file_name)
 
-	# we don't need the audio file any more - let's delete it!
-	os.remove(file_name)
+    # we don't need the audio file any more - let's delete it!
+    os.remove(file_name)
 
-	# send back result as a json file
-	result = {"keyword": pred_word}
-	return jsonify(result)
+    # send back result as a json file
+    result = {"keyword": pred_word}
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=False)
