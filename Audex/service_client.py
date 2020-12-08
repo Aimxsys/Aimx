617b8a6e74ebd762fef9ac67789f234d85a3a44a
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-12-08 12:52:31 +0400

    Rename + Comment-

--- Audex/service_client.py
+++ Audex/service_client.py
@@ -1,7 +1,6 @@
 import requests
 
-# server url
-URL = "http://127.0.0.1:5000/predict"
+SERVER_URL = "http://127.0.0.1:5000/predict"
 
 # audio file we'd like to send for predicting keyword
 AUDIO_FILE_PATH = "../workdir/test/ds_down_1.wav"
@@ -15,7 +14,7 @@ if __name__ == "__main__":
     payload = {"file": (AUDIO_FILE_PATH, audio_file, "audio/wav")}
 
     # send the package
-    response      = requests.post(URL, files=payload)
+    response      = requests.post(SERVER_URL, files=payload)
     response_data = response.json()
 
     print("Predicted keyword: {}".format(response_data["pred_word"]))
