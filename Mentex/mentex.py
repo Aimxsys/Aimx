aa574f4ae8414adfb9f8d6f8daa797c3b9da1293
annaagh <anna.aghabalyann@gmail.com>
2020-12-15 18:07:28 +0400

    PC comment fix

--- Mentex/mentex.py
+++ Mentex/mentex.py
@@ -5,5 +5,5 @@ import librosa
 
 parser = argparse.ArgumentParser(description="This utility creates new audio files by manipulating given audio.")
 parser.add_argument("-in", default='../working_dir', type=str, help="Relative path to the audio file directory.")
-parser.add_argument("-out", default='../working_dir', type=str, help="Relative path to the audio file directory.")
+parser.add_argument("-out", default='../working_dir', type=str, help="Relative path to the output directory.")
 args = parser.parse_args()
