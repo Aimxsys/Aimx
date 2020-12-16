7ba91ef89b3362512572a0a51bb2600f14462538
annaagh <anna.aghabalyann@gmail.com>
2020-12-16 16:12:33 +0400

    Play audio

--- Mentex/mentex.py
+++ Mentex/mentex.py
@@ -25,12 +25,12 @@ class CMentex(object):
     def load_audio_file(self):
 
         # TODO: Hard coded length
-        input_length = 16000
+        self.input_length = 16000
         data = librosa.core.load(self.args.input_file)[0]
-        if len(data) > input_length:
-            data = data[:input_length]
+        if len(data) > self.input_length:
+            data = data[:self.input_length]
         else:
-            data = np.pad(data, (0, max(0, input_length - len(data))), "constant")
+            data = np.pad(data, (0, max(0, self.input_length - len(data))), "constant")
         return data
 
     def plot_time_series(self, data):
@@ -40,10 +40,13 @@ class CMentex(object):
         plt.plot(np.linspace(0, 1, len(data)), data)
         plt.show()
 
+    def play_audio(self, data):
+        ipd.Audio(data, rate=self.input_length)
+
     def run(self):
         data = self.load_audio_file()
         self.plot_time_series(data)
-        ipd.Audio(data, rate=16000)
+        ipd.Audio(data, rate=self.input_length)
 
 
 if __name__ == "__main__":
