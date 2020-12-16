124453e064050ac4845c292e8bb4977ea4101f25
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-12-16 20:52:28 +0400

    Docker install & setup file for later possible use in cloud servers

---
+++ Audex/init_server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Dec 2020: Template file for possible later usage
+
+sudo apt-get update
+
+# install docker
+sudo apt install docker.io
+
+# start docker service
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# install docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# build and run docker containers
+cd ~/Audex/server
+sudo docker-compose up --build
