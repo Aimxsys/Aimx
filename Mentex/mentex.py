abcc397409d94c4f59a84d8c5961a7a7d552260d
annaagh <anna.aghabalyann@gmail.com>
2020-12-16 16:07:32 +0400

    Plot audio, run function

--- Mentex/mentex.py
+++ Mentex/mentex.py
@@ -3,6 +3,8 @@
 import argparse
 import librosa
 import numpy as np
+import IPython.display as ipd
+import matplotlib.pyplot as plt
 
 
 class CMentex(object):
@@ -13,7 +15,7 @@ class CMentex(object):
 
     def parser(self):
 
-        parser = argparse.ArgumentParser(description="This utility creates new audio files by manipulating given audio.")
+        parser = argparse.ArgumentParser(description="This utility creates new audio files by manipulating given audio")
         parser.add_argument("-in", dest='input_file', default='../working_dir',
                             type=str, help="Relative path to the audio file.")
         parser.add_argument("-out", dest='output_path', default='../working_dir',
@@ -21,10 +23,29 @@ class CMentex(object):
         self.args = parser.parse_args()
 
     def load_audio_file(self):
+
+        # TODO: Hard coded length
         input_length = 16000
-        data = librosa.core.load(self.args.input_file)[0]  #, sr=16000
+        data = librosa.core.load(self.args.input_file)[0]
         if len(data) > input_length:
             data = data[:input_length]
         else:
             data = np.pad(data, (0, max(0, input_length - len(data))), "constant")
         return data
+
+    def plot_time_series(self, data):
+        fig = plt.figure(figsize=(14, 8))
+        plt.title('Raw wave ')
+        plt.ylabel('Amplitude')
+        plt.plot(np.linspace(0, 1, len(data)), data)
+        plt.show()
+
+    def run(self):
+        data = self.load_audio_file()
+        self.plot_time_series(data)
+        ipd.Audio(data, rate=16000)
+
+
+if __name__ == "__main__":
+    mentex = CMentex()
+    mentex.run()
