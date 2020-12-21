7977f130b9889f470b5b7de502d9469e10014a36
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-12-21 21:17:20 +0400

    Added bat files for quickly switching between models

---
+++ Audex/setmodel_asr_e50_3v_1000d.bat
@@ -0,0 +1,3 @@
+xcopy "..\workdir\gen_models\model_cnn_e50_3v_1000d_speech_commands_v001_13m_2048w_512h_5i_22050r_1s\assets\*.json"^
+      "..\workdir\"^
+	  /K /H /Y
---
+++ Audex/setmodel_genre_e50_10v_100d.bat
@@ -0,0 +1,3 @@
+xcopy "..\workdir\gen_models\model_ann_e50_10v_100d_dataset_c10_f100_13m_2048w_512h_5i_22050r_30s\assets\*.json"^
+      "..\workdir\"^
+	  /K /H /Y
