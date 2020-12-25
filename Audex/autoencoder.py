a770b0c97c4993a40b6920d43eb2e7db12d3b651
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-12-25 16:13:25 +0400

    Move

--- Audex/autoencoder.py
+++ Audex/autoencoder.py
@@ -33,6 +33,12 @@ class Autoencoder:
         self._build_decoder()
         #self._build_autoencoder()
 
+    def _build_encoder(self):
+        encoder_input         = self._add_encoder_input()
+        conv_layers           = self._add_conv_layers(encoder_input)
+        bottleneck            = self._add_bottleneck(conv_layers)
+        self.encoder          = Model(encoder_input, bottleneck, name="encoder")
+
     def _build_decoder(self):
         decoder_input         = self._add_decoder_input()
         dense_layer           = self._add_dense_layer(decoder_input)
@@ -40,6 +46,38 @@ class Autoencoder:
         conv_transpose_layers = self._add_conv_transpose_layers(reshape_layer)
         decoder_output        = self._add_decoder_output(conv_transpose_layers)
         self.decoder          = Model(decoder_input, decoder_output, name="decoder")
+
+    def _add_encoder_input(self):
+        return Input(shape=self.input_shape, name="encoder_input")
+
+    def _add_conv_layers(self, encoder_input):
+        """ Create all convolutional blocks in encoder. """
+        x = encoder_input
+        for layer_index in range(self._num_conv_layers):
+            x = self._add_conv_layer(layer_index, x)
+        return x
+
+    def _add_conv_layer(self, layer_index, x):
+        """ Add a convolutional block to a graph of layers, consisting of conv 2d + ReLU + batch normalization. """
+        layer_number = layer_index + 1
+        conv_layer = Conv2D(
+            filters     = self.conv_filters[layer_index],
+            kernel_size = self.conv_kernels[layer_index],
+            strides     = self.conv_strides[layer_index],
+            padding     = "same",
+            name        = f"encoder_conv_layer_{layer_number}"
+        )
+        x = conv_layer(x)
+        x = ReLU(              name = f"encoder_relu_{layer_number}")(x)
+        x = BatchNormalization(name = f"encoder_bn_{layer_number}")(x)
+        return x
+
+    def _add_bottleneck(self, x):
+        """ Flatten data and add bottleneck (Dense layer). """
+        self._shape_before_bottleneck = K.int_shape(x)[1:]
+        x = Flatten()(x)
+        x = Dense(self.latent_space_dim, name="encoder_output")(x)
+        return x
 
     def _add_decoder_input(self):
         return Input(shape=self.latent_space_dim, name="decoder_input")
@@ -84,44 +122,6 @@ class Autoencoder:
         x = conv_transpose_layer(x)
         output_layer = Activation("sigmoid", name="sigmoid_layer")(x)
         return output_layer
-
-    def _build_encoder(self):
-        encoder_input = self._add_encoder_input()
-        conv_layers   = self._add_conv_layers(encoder_input)
-        bottleneck    = self._add_bottleneck(conv_layers)
-        self.encoder  = Model(encoder_input, bottleneck, name="encoder")
-
-    def _add_encoder_input(self):
-        return Input(shape=self.input_shape, name="encoder_input")
-
-    def _add_conv_layers(self, encoder_input):
-        """ Create all convolutional blocks in encoder. """
-        x = encoder_input
-        for layer_index in range(self._num_conv_layers):
-            x = self._add_conv_layer(layer_index, x)
-        return x
-
-    def _add_conv_layer(self, layer_index, x):
-        """ Add a convolutional block to a graph of layers, consisting of conv 2d + ReLU + batch normalization. """
-        layer_number = layer_index + 1
-        conv_layer = Conv2D(
-            filters     = self.conv_filters[layer_index],
-            kernel_size = self.conv_kernels[layer_index],
-            strides     = self.conv_strides[layer_index],
-            padding     = "same",
-            name        = f"encoder_conv_layer_{layer_number}"
-        )
-        x = conv_layer(x)
-        x = ReLU(name = f"encoder_relu_{layer_number}")(x)
-        x = BatchNormalization(name = f"encoder_bn_{layer_number}")(x)
-        return x
-
-    def _add_bottleneck(self, x):
-        """ Flatten data and add bottleneck (Dense layer). """
-        self._shape_before_bottleneck = K.int_shape(x)[1:]
-        x = Flatten()(x)
-        x = Dense(self.latent_space_dim, name="encoder_output")(x)
-        return x
 
 if __name__ == "__main__":
     autoencoder = Autoencoder(
