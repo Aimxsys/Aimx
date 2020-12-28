dd6f0c7e363c7ecbbd46cf116f2a2e44cf091146
Leo Heinsaar <leo.heinsaar@hotmail.com>
2020-12-28 17:06:01 +0400

    Space

--- Audex/rt_mic_plot.py
+++ Audex/rt_mic_plot.py
@@ -119,9 +119,9 @@ try:
     fig.tight_layout(pad=0)
 
     input_stream = sd.InputStream(device     = args.device,
-                                  #blocksize  = 0, # Number of frames passed to audio_callback(), i.e. granularity for a blocking r/w stream.
-                                                   # Default and special value 0 means audio_callback() will receive an optimal (and possibly
-                                                   # varying) number of frames based on host requirements and the requested latency settings.
+                                  #blocksize = 0, # Number of frames passed to audio_callback(), i.e. granularity for a blocking r/w stream.
+                                                  # Default and special value 0 means audio_callback() will receive an optimal (and possibly
+                                                  # varying) number of frames based on host requirements and the requested latency settings.
                                   channels   = max(args.channels),
                                   samplerate = args.samplerate,
                                   callback   = audio_callback)
