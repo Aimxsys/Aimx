0595d7729bdad0687da92284d4ea270a7ab6603f
Leo Heinsaar <leo.heinsaar@hotmail.com>
2021-01-11 11:48:54 +0400

    Comment

--- Audex/rt_mic_spectr.py
+++ Audex/rt_mic_spectr.py
@@ -50,7 +50,7 @@ if high <= low:
     parser.error('HIGH must be greater than LOW')
 
 # Create a nice output gradient using ANSI escape sequences.
-# Stolen from https://gist.github.com/maurisvh/df919538bcef391bc89f
+# From https://gist.github.com/maurisvh/df919538bcef391bc89f
 colors   = 30, 34, 35, 91, 93, 97
 chars    = ' :%#\t#%:'
 gradient = []
