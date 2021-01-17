687a4ca169cd7488ce26fa659a348ee2d1ad5401
Leo Heinsaar <leo.heinsaar@hotmail.com>
2021-01-17 20:00:52 +0400

    Completed AE & initial commit of its training

--- Audex/autoencoder.py
+++ Audex/autoencoder.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization, Flatten, Dense, Reshape, Conv2DTranspose, Activation
-from tensorflow.keras        import backend as K
+from tensorflow.keras.layers     import Input, Conv2D, ReLU, BatchNormalization, Flatten, Dense, Reshape, Conv2DTranspose, Activation
+from tensorflow.keras            import backend as K
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses     import MeanSquaredError
 
 import numpy as np
 
@@ -32,22 +34,33 @@ class Autoencoder:
 
         self._num_conv_layers = len(conv_filters)
         self._shape_before_bottleneck = None
+        self._model_input             = None
 
         self._build()
 
     def summary(self):
         self.encoder.summary()
         self.decoder.summary()
+        self.model.summary()
+
+    def compile(self, learning_rate=0.0001):
+        opt = Adam(learning_rate=learning_rate)
+        mse = MeanSquaredError()
+        self.model.compile(optimizer=opt, loss=mse)
+
+    def train(self, x_train, batch_size, num_epochs):
+        self.model.fit(x_train, x_train, batch_size=batch_size, epochs=num_epochs, shuffle=True)
 
     def _build(self):
         self._build_encoder()
         self._build_decoder()
-        #self._build_autoencoder()
+        self._build_autoencoder()
 
     def _build_encoder(self):
         encoder_input         = self._add_encoder_input()
         conv_layers           = self._add_conv_layers(encoder_input)
         bottleneck            = self._add_bottleneck(conv_layers)
+        self._model_input     = encoder_input
         self.encoder          = tf.keras.Model(encoder_input, bottleneck, name="encoder")
 
     def _build_decoder(self):
@@ -57,6 +70,11 @@ class Autoencoder:
         conv_transpose_layers = self._add_conv_transpose_layers(reshape_layer)
         decoder_output        = self._add_decoder_output(conv_transpose_layers)
         self.decoder          = tf.keras.Model(decoder_input, decoder_output, name="decoder")
+
+    def _build_autoencoder(self):
+        model_input  = self._model_input
+        model_output = self.decoder(self.encoder(model_input))
+        self.model   = tf.keras.Model(model_input, model_output, name="autoencoder")
 
     def _add_encoder_input(self):
         return tf.keras.layers.Input(shape=self.input_shape, name="encoder_input")
---
+++ Audex/train_autoencoder.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+  
+from tensorflow.keras.datasets import mnist
+from autoencoder import Autoencoder
+
+from pathlib  import Path
+from datetime import timedelta
+import time
+import argparse
+import numpy as np
+import cmd
+import sys
+import os
+
+from sklearn.model_selection import train_test_split
+import tensorflow.keras as keras
+
+# Add this directory to path so that package is recognized.
+# Looks like a hack, but is ok for now to allow moving forward.
+# Source: https://stackoverflow.com/a/23891673/4973224
+# TODO: Replace with the idiomatic way.
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from Audex.utils.utils_audex import *
+
+def process_clargs():
+    # Calling with "-traindata_path /to/file" will expect to find the file in ./to directory.
+    parser = argparse.ArgumentParser(description = 'This utility script allows you to experiment with'
+                                                   ' audio files and their various spectrograms.')
+
+    parser.add_argument("-traindata_path", default=Aimx.MOST_RECENT_OUTPUT, type = Path,
+                        help = 'Path to the data file to be fed to the NN. Or use ' + Aimx.MOST_RECENT_OUTPUT +
+                               ', which by design is the output of the previous step of dataset preprocessing.')
+
+    parser.add_argument("-batch_size", default = 32,    type=int, help = 'Batch size.')
+    parser.add_argument("-epochs",     default = 50,    type=int, help = 'Number of epochs to train.')
+    parser.add_argument("-patience",   default = 10,    type=int, help = 'Number of epochs with no improvement after which training will be stopped.')
+    parser.add_argument("-verbose",    default =  1,    type=int, help = 'Verbosity modes: 0 (silent), 1 (will show progress bar),'
+                                                                         ' or 2 (one line per epoch). Default is 1.')
+    parser.add_argument("-showplot",   action ='store_true',      help = 'At the end, will show an interactive plot of the training history.')
+    parser.add_argument("-savemodel",  action ='store_true',      help = 'Save a trained model in directory ' + quote(Aimx.Paths.GEN_SAVED_MODELS))
+    parser.add_argument("-example",    action ='store_true',      help = 'Show a working example on how to call the script.')
+
+    args = parser.parse_args()
+
+    ########################## Command Argument Handling & Verification #######################
+
+    if args.example:
+        print_info(nameofthis(__file__) + " -epochs 5")
+        exit()
+
+    if provided(args.traindata_path) and not args.traindata_path.exists():
+        if str(args.traindata_path) is not Aimx.MOST_RECENT_OUTPUT:
+            raise FileNotFoundError("Directory " + quote(pinkred(os.getcwd())) + " does not contain requested path " + quote(pinkred(args.traindata_path)))
+
+    # path to the traindata file that stores MFCCs and genre labels for each processed segment
+    args.traindata_path = get_actual_traindata_path(args.traindata_path)
+
+    if not args.savemodel and os.path.getsize(args.traindata_path) > 50_000_000: # > 50 Mb
+        args.savemodel = prompt_user_warning("Attempting to train on a large >50Mb traindata without '-savemodel',"
+                                             " would you rather save the final model? [yes / no] ")
+        print_info("As requested, proceeding with -savemodel =", args.savemodel)
+    
+    ###########################################################################################
+    
+    print_script_start_preamble(nameofthis(__file__), vars(args))
+
+    return args
+
+LEARNING_RATE = 0.0005
+
+def load_mnist():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train = x_train.astype("float32") / 255
+    x_train = x_train.reshape(x_train.shape + (1,))
+    x_test  = x_test.astype("float32") / 255
+    x_test  = x_test.reshape(x_test.shape + (1,))
+
+    return x_train, y_train, x_test, y_test
+
+def train(x_train, learning_rate, batch_size, epochs):
+    autoencoder = Autoencoder(
+        input_shape      = (28, 28, 1),
+        conv_filters     = (32, 64, 64, 64), # 4 conv layers each with the corresponding number of filters
+        # len() of tuples below must be at least that of the above, like here they are both of len() 4. Otherwise you'll get an error.
+        conv_kernels     = (3, 3, 3, 3),
+        conv_strides     = (1, 2, 2, 1),     # stride 2 in conv layers means downsampling (halving) at that point
+        latent_space_dim = 2
+    )
+    autoencoder.summary()
+    autoencoder.compile(learning_rate)
+    autoencoder.train(x_train, batch_size, epochs)
+    return autoencoder
+
+if __name__ == "__main__":
+    args = process_clargs()
+    x_train, _, _, _ = load_mnist()
+    autoencoder = train(x_train[:5000], LEARNING_RATE, args.batch_size, args.epochs)
