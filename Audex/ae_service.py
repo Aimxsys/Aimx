a44ede9e3db6f5a083b45cf929c4e4079939d42d
Leo Heinsaar <leo.heinsaar@hotmail.com>
2021-01-22 20:19:17 +0400

    PC+ apply -model_path

--- Audex/ae_service.py
+++ Audex/ae_service.py
@@ -81,7 +81,7 @@ def plot_images_encoded_in_latent_space(latent_reps, sample_labels):
 
 if __name__ == "__main__":
     args = process_clargs()
-    ae = Autoencoder.load()
+    ae = Autoencoder.load_custom(args.model_path)
     x_train, y_train, x_test, y_test = load_mnist()
 
     sample_images, _ = select_images(x_test, y_test, args.num_genims)
