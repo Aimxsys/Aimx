cfb8440684f1550042eb8483db53e418e3a848fb
Leo Heinsaar <leo.heinsaar@hotmail.com>
2021-01-26 12:50:45 +0400

    Rename clarg -num_samples to -num_infers

--- Audex/ae_service.py
+++ Audex/ae_service.py
@@ -27,7 +27,7 @@ def process_clargs():
     parser.add_argument("-inferdata_range", default=[0, 50], nargs='*', type = int,  help = 'Range in -inferdata_path on which to do inference.')
 
     parser.add_argument("-repeat",      default =  1,                   type = int,  help = 'Repeat the run of the service specified number of times.')
-    parser.add_argument("-num_samples", default = 10,                   type = int,  help = 'Number of images to generate. If small, will also plot latent space points.')
+    parser.add_argument("-num_infers",  default = 10,                   type = int,  help = 'Number of images to generate. If small, will also plot latent space points.')
     parser.add_argument("-randomize",  action ='store_true',                         help = 'Randomize picking from the dataset.')
     parser.add_argument("-showvencs",  action ='store_true',                         help = 'At the end, will show vencs in an interactive window.')
     parser.add_argument("-showgenims", action ='store_true',                         help = 'At the end, will show genims in an interactive window.')
@@ -155,12 +155,12 @@ if __name__ == "__main__":
 
         # Generate images from latent space vencs             <| 
         if args.mode_gen:
-            genims = ae.gen_random(args.num_samples)
+            genims = ae.gen_random(args.num_infers)
             plot_genims(genims, extract_filename(args.model_path), args.showgenims)
 
         # Regenerate images from selected dataset samples   |><|
         elif args.mode_regen:
-            sample_images, sample_labels = pick_images(x_test, y_test, args.num_samples, args.randomize)
+            sample_images, sample_labels = pick_images(x_test, y_test, args.num_infers, args.randomize)
  
             vencs, genims = ae.regen(sample_images)
  
