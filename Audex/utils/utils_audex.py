2be64f735620dc38067dbd87be23016f66280ef9
Leo Heinsaar <leo.heinsaar@hotmail.com>
2021-01-27 13:51:11 +0400

    PC+ which model was saved

--- Audex/utils/utils_audex.py
+++ Audex/utils/utils_audex.py
@@ -204,7 +204,7 @@ def save_model(model, trainid):
     MODEL_FULLPATH = os.path.join(Aimx.Paths.GEN_SAVED_MODELS, "model_" + trainid)
     print_info("|||||| Saving model", quote_path(MODEL_FULLPATH), "... ", end="")
     model.save(MODEL_FULLPATH)
-    print_info("[DONE]")
+    print_info("[DONE (model saved)]", quote_path(MODEL_FULLPATH)) # double mention since TF's warnings interfere
     print_info("|||||| Copying file", quote_path(Aimx.Dataprep.RESULT_METADATA_FULLPATH), "into model assets... ", end="")
     copy2(Aimx.Dataprep.RESULT_METADATA_FULLPATH, os.path.join(MODEL_FULLPATH, "assets"))
     print_info("[DONE]")
