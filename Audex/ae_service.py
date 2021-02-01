6792590b202b2b5a38f095087bd7f9f02858e6b2
Leo Heinsaar <leo.heinsaar@hotmail.com>
2021-02-01 21:25:15 +0400

    Initial commit of ae_service.py

---
+++ Audex/ae_service.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as pt
+import argparse
+import sys
+import os
+
+from itertools import islice
+
+from ae             import Autoencoder
+from ae_mnist_train import normalize_traindata_pixels
+from ae_mnist_train import reshape_traindata
+
+# Add this directory to path so that package is recognized.
+# Looks like a hack, but is ok for now to allow moving forward.
+# Source: https://stackoverflow.com/a/23891673/4973224
+# TODO: Replace with the idiomatic way.
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+from Audex.utils.utils_audex import *
+from asr_service import CreateAsrService
+
+def process_clargs():
+    # Calling with "-traindata_path /to/file" will expect to find the file in ./to directory.
+    parser = argparse.ArgumentParser(description = '[TODO: Script description].')
+
+    parser.add_argument("-model_path", default=Aimx.MOST_RECENT_OUTPUT, type = Path, help = 'Path to the model to be loaded.')
+    parser.add_argument("-inferdata_path",                              type = Path, help = 'Path to the audio files on which model inference is to be tested.')
+    parser.add_argument("-inferdata_range", default=[0, 50], nargs='*', type = int,  help = 'Range in -inferdata_path on which to do inference.')
+
+    parser.add_argument("-repeat",      default =  1,                   type = int,  help = 'Repeat the run of the service specified number of times.')
+    parser.add_argument("-num_infers",  default = 10,                   type = int,  help = 'Number of images to generate. If small, will also plot latent space points.')
+    parser.add_argument("-randomize",  action ='store_true',                         help = 'Randomize picking from the dataset.')
+    parser.add_argument("-showvencs",  action ='store_true',                         help = 'At the end, will show vencs in an interactive window.')
+    parser.add_argument("-showgenums", action ='store_true',                         help = 'At the end, will show genums in an interactive window.')
+    parser.add_argument("-mode_gen",   action ='store_true',                         help = 'This mode will generate a genum from latent space.')
+    parser.add_argument("-mode_regen", action ='store_true',                         help = 'This mode will regenerate an image.')
+
+    parser.add_argument("-n_mfcc",        type=int, default = 13,                    help = 'Number of MFCC to extract.')
+    parser.add_argument("-n_fft",         type=int, default = 2048,                  help = 'Length of the FFT window.   Measured in # of samples.')
+    parser.add_argument("-hop_length",    type=int, default = 512,                   help = 'Sliding window for the FFT. Measured in # of samples.')
+    parser.add_argument("-sample_rate",   type=int, default = 22050,                 help = 'Sample rate at which to read the audio files.')
+    parser.add_argument("-load_duration", type=int, default = 1,                     help = 'Only load up to this much audio (in seconds).')
+
+    parser.add_argument("-example",    action ='store_true',                         help = 'Show a working example on how to call the script.')
+
+    args = parser.parse_args()
+
+    ########################## Command Argument Handling & Verification #######################
+
+    if args.example:
+        print_info(nameofthis(__file__) + "[TODO: REPLACE THIS WITH AN ACTUAL EXAMPLE]")
+        exit()
+
+    if provided(args.inferdata_path) and not args.inferdata_path.exists():
+        raise FileNotFoundError("Directory " + quote(pinkred(os.getcwd())) + " does not contain requested path " + quote(pinkred(args.inferdata_path)))
+
+    args.model_path = get_actual_model_path(args.model_path)
+
+    args.n_mfcc        = get_training_result_meta()[Aimx.Dataprep.SIGNAL_NUMERIZATION_PARAMS]["n_mfcc"]        if not provided(args.n_mfcc)        else args.n_mfcc
+    args.n_fft         = get_training_result_meta()[Aimx.Dataprep.SIGNAL_NUMERIZATION_PARAMS]["n_fft"]         if not provided(args.n_fft)         else args.n_fft
+    args.n_hop_length  = get_training_result_meta()[Aimx.Dataprep.SIGNAL_NUMERIZATION_PARAMS]["hop_length"]    if not provided(args.hop_length)    else args.hop_length
+    args.sample_rate   = get_training_result_meta()[Aimx.Dataprep.SIGNAL_NUMERIZATION_PARAMS]["sample_rate"]   if not provided(args.hop_length)    else args.sample_rate
+    args.load_duration = get_training_result_meta()[Aimx.Dataprep.SIGNAL_NUMERIZATION_PARAMS]["load_duration"] if not provided(args.load_duration) else args.load_duration
+    
+    ###########################################################################################
+    
+    print_script_start_preamble(nameofthis(__file__), vars(args))
+
+    return args, parser
+
+def pick_from(images, labels, num_samples=10, randomize=True):
+    if randomize:
+        indexes = np.random.choice(range(len(images)), num_samples)
+    else:
+        indexes = np.arange(num_samples)
+    sample_images = images[indexes] # num_samples images
+    sample_labels = labels[indexes] # num_samples labels
+    return sample_images, sample_labels
+
+def plot_vencs(vencs, labels, modelname, showinteractive):
+    dim_latent = vencs.shape[1]
+    pt.figure(figsize=(10, 10))
+
+    # Print encodings if not too many
+    if len(vencs) < 20:
+        print_info("Digits and their corresponding {}-d vencs:".format(vencs.shape[1]))
+        for i in range(len(vencs)):
+            print(cyan(labels[i] if labels is not None else "None"), np.around(vencs[i], 2))
+
+    # Scatterplot first two coordinates of the vencs in the latent space,
+    # which will be the exact representation in case the latent space is two-dimensional.
+    # Note that to map the venc to its digit, look at the corresponding color on the colormap.
+    print_info("Scatter-plotting first two coordinates of the {}-d vencs...".format(vencs.shape[1]))
+    pt.scatter(vencs[:, 0], vencs[:, 1], cmap="rainbow", c=labels, alpha=0.5, s=2)
+    if labels is not None:
+        pt.colorbar()
+
+    # save the plot as most recent (often useful when comparing to a next NN run)
+    Path(Aimx.Paths.GEN_PLOTS_VENCS).mkdir(parents=True, exist_ok=True)
+    VENCS_FULLPATH = os.path.join(Aimx.Paths.GEN_PLOTS_VENCS, modelname + ".png")
+    print_info("|||||| Saving file", quote_path(VENCS_FULLPATH), "... ", end="")
+    pt.savefig(VENCS_FULLPATH)
+    print_info("[DONE]")
+
+    if showinteractive:
+        pt.show()
+    else:
+        pt.close()
+
+def plot_regenums(genums, origimages, modelname, showinteractive):
+    fig = pt.figure(figsize=(15, 3))
+
+    num_images = len(origimages)
+    if num_images > 100: return # too many genums, takes long to plot and indistinguishable to human eye
+    for i, (origimage, genum) in enumerate(zip(origimages, genums)):
+        
+        # Original image
+        origimage = origimage.squeeze()
+        ax = fig.add_subplot(2, num_images, i + 1)
+        ax.axis("off")
+        ax.imshow(origimage, cmap="gray_r")
+
+        # Genum
+        genum = genum.squeeze() # (28, 28, 1) ===> (28, 28)
+        ax = fig.add_subplot(2, num_images, i + num_images + 1)
+        ax.axis("off")
+        ax.imshow(genum, cmap="gray_r")
+
+    # save the plot as most recent (often useful when comparing to a next NN run)
+    Path(Aimx.Paths.GEN_PLOTS_GENUM).mkdir(parents=True, exist_ok=True)
+    GENUM_FULLPATH = os.path.join(Aimx.Paths.GEN_PLOTS_GENUM, modelname + ".png")
+    print_info("|||||| Saving file", quote_path(GENUM_FULLPATH), "... ", end="")
+    pt.savefig(GENUM_FULLPATH)
+    print_info("[DONE]")
+
+    if showinteractive:
+        pt.show()
+    else:
+        pt.close()
+
+def plot_genums(genums, modelname, showinteractive):
+    fig = pt.figure(figsize=(15, 3))
+
+    num_genums = len(genums)
+    if num_genums > 100: return # too many genums, takes long to plot and indistinguishable to human eye
+    for i, genum in enumerate(genums):
+        
+        genum = genum.squeeze()
+        ax = fig.add_subplot(2, num_genums, i + num_genums + 1)
+        ax.axis("off")
+        ax.imshow(genum, cmap="gray_r")
+
+    # save the plot as most recent (often useful when comparing to a next NN run)
+    Path(Aimx.Paths.GEN_PLOTS_GENUM).mkdir(parents=True, exist_ok=True)
+    GENUM_FULLPATH = os.path.join(Aimx.Paths.GEN_PLOTS_GENUM, modelname + ".png")
+    print_info("|||||| Saving file ", quote_path(GENUM_FULLPATH), "... ", end="")
+    pt.savefig(GENUM_FULLPATH)
+    print_info("[DONE]")
+
+    if showinteractive:
+        pt.show()
+    else:
+        pt.close()
+
+if __name__ == "__main__":
+    args, parser = process_clargs()
+
+#####################################################################################################
+
+#    model = Autoencoder.load_model(args.model_path)
+#
+#    (_, _), (x_test, y_test) = mnist.load_data() # traindata                      x_train.shape == (60000, 28, 28)
+#    _,  _,   x_test, y_test  = reshape_traindata(         _, _, x_test, y_test) # x_train.shape == (60000, 28, 28, 1)
+#    _,  _,   x_test, y_test  = normalize_traindata_pixels(_, _, x_test, y_test)
+#
+#    # MNIST traindata values:
+#    # x_test.shape == (10000, 28, 28, 1)
+#    # y_test.shape == (10000,)
+#
+#    while args.repeat > 0:
+#        args.repeat -= 1
+#
+#        # Generate images from latent space vencs             <| 
+#        if args.mode_gen:
+#            labels = None # signify "unknown" (whatever the genum turns out to be)
+#
+#            vencs, genums = model.gen_random(args.num_infers)
+#
+#            plot_vencs(vencs, labels, extract_filename(args.model_path), args.showvencs)
+#            plot_genums(genums,       extract_filename(args.model_path), args.showgenums)
+#
+#        # Regenerate images from selected dataset samples   |><|
+#        elif args.mode_regen:
+#            sample_images, sample_labels = pick_from(x_test, y_test, args.num_infers, args.randomize)
+# 
+#            vencs, genums = model.regen(sample_images)
+# 
+#            plot_vencs(vencs,     sample_labels, extract_filename(args.model_path), args.showvencs)
+#            plot_regenums(genums, sample_images, extract_filename(args.model_path), args.showgenums)
+
+#####################################################################################################
+
+    asr = CreateAsrService(args.model_path)
+    
+    print_info("\nPredicting with dataset view (labels):", asr.label_mapping)
+    print_info("On files in:", args.inferdata_path)
+    print_info(asr.inference_report_headers.format("Loaded Sec", "Con", "Filename", "Inference"))
+
+    (_, _, afnames) = next(os.walk(args.inferdata_path))
+    
+    START = args.inferdata_range[0]; # of the range in -inferdata_path on which to do inference
+    END   = args.inferdata_range[1]; # of the range in -inferdata_path on which to do inference
+
+    for i, afname in enumerate(islice(afnames, START, END)):
+        af_fullpath = os.path.join(args.inferdata_path, afname)
+        asr.load_audiofile(af_fullpath, args.load_duration)
+        if len(asr.af_signal) < args.sample_rate: # process only signals of at least 1 sec
+            print_info("skipped a short (< 1s) signal")
+            continue
+        for i in range(int(asr.af_loaded_duration)):
+            signums = asr.numerize(startsec=i, n_mfcc=args.n_mfcc, n_fft=args.n_fft, hop_length=args.hop_length)
+            deprint(signums.shape, yellow("signums.shape"))
+            deprint(signums[0][0], yellow("signums[0][0]"))
+            w, c = asr.predict(signums)
+            asr.report(w, c, 0.9)
