6356a756be7ebb9aae46229c2bd2af512300530c
Leo Heinsaar <leo.heinsaar@hotmail.com>
2021-02-01 13:04:25 +0400

    Swap words in mnist file names

--- Audex/ae_mnist_service.py
+++ Audex/ae_mnist_service.py
@@ -8,8 +8,8 @@ import sys
 import os
 
 from ae             import Autoencoder
-from ae_train_mnist import normalize_traindata_pixels
-from ae_train_mnist import reshape_traindata
+from ae_mnist_train import normalize_traindata_pixels
+from ae_mnist_train import reshape_traindata
 
 # Add this directory to path so that package is recognized.
 # Looks like a hack, but is ok for now to allow moving forward.
