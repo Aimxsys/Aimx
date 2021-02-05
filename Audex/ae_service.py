19f4b3e182b98a577ec870d39137a34e91efbdfa
Leo Heinsaar <leo.heinsaar@hotmail.com>
2021-02-05 15:58:54 +0400

    PC* "Continue on to numerize?"

--- Audex/ae_service.py
+++ Audex/ae_service.py
@@ -192,7 +192,7 @@ if __name__ == "__main__":
         # Play original sound
         play(asr.af_signal, asr.af_sr,
              "Playing original audio signal {} of shape {} and numerical content:".format(quote(cyan(afname)), cyan(asr.af_signal.shape)),
-             "Continue to numerize?")
+             "Continue on to numerize?")
                 
         # Numerize original sound for inference. signums.shape will be:
         # (1, 44,  16, 1) if MFCC
