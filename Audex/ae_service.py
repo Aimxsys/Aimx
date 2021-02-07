cc413207173a8faad1b50f116c000a6d8d15c7c4
Leo Heinsaar <leo.heinsaar@hotmail.com>
2021-02-07 19:43:49 +0400

    Message

--- Audex/ae_service.py
+++ Audex/ae_service.py
@@ -206,7 +206,7 @@ if __name__ == "__main__":
         # Restore and play back immediately to compare with the original playback
         #                                            squeeze()  transpose()   to_audio()
         # by transforming mel-signumerization: (1, 44, 128, 1) => (44, 128) => (128, 44) => (22016,)
-        print_info("Shape of signumerization immediately restored for playback:", pinkred(signums.shape))
+        print_info("Immediately restoring for playback signums.shape:", pinkred(signums.shape))
         #signal_restored = librosa.feature.inverse.mfcc_to_audio(signums.squeeze().T)
         signal_restored = librosa.feature.inverse.mel_to_audio(signums.squeeze().T)
         signal_restored = np.pad(signal_restored, pad_width=(0, len(asr.af_signal) - len(signal_restored)))
